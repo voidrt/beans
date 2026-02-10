b7012279c34b748ce970e5e9df219e222794cabc
voidrt <edbracaioli@gmail.com>
2026-02-09 21:59:51 -0300

    ...

---
+++ mango/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+swaybg -i ~/Pictures/wallpapers/oxycarbon-waves.png >/dev/null 2>&1 &
+qs -c noctalia-shell &
+/usr/bin/gentoo-pipewire-launcher &
---
+++ mango/reload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+killall waybar
+waybar
+
+/usr/bin/gentoo-pipewire-launcher &
